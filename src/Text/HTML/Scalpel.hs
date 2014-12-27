7bae28d7a6aa64d276fb78700950c88573e8ac75
Will Coster <willcoster@gmail.com>
2014-12-26 18:43:47 -0800

    All basic scrapers working

--- src/Text/HTML/Scalpel.hs
+++ src/Text/HTML/Scalpel.hs
@@ -1,10 +1,5 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE ParallelListComp #-}
-{-# LANGUAGE PatternGuards #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeFamilies #-}
 module Text.HTML.Scalpel (
     Selector
 ,   Selectable (..)
@@ -14,10 +9,18 @@ module Text.HTML.Scalpel (
 ,   select
 
 ,   Scraper (..)
+,   attr
+,   attrs
+,   text
+,   texts
+,   chroot
+,   chroots
 ) where
 
 import Control.Applicative
+import Control.Monad
 import Data.List
+import Data.Maybe
 
 import qualified Data.Text as T
 import qualified Text.HTML.TagSoup as TagSoup
@@ -29,13 +32,13 @@ import qualified Text.StringLike as TagSoup
 
 type Selector str = [SelectNode str]
 
-class Selectable s str | s -> str where
+class Selectable str s | s -> str where
     toSelector :: s -> Selector str
 
 data SelectNode str = SelectNode str [TagSoup.Attribute str]
     deriving (Show)
 
-instance Selectable (Selector str) str where
+instance Selectable str (Selector str) where
     toSelector = id
 
 instance Selectable T.Text T.Text where
@@ -44,7 +47,7 @@ instance Selectable T.Text T.Text where
 instance Selectable String String where
     toSelector node = [SelectNode node []]
 
-select :: (TagSoup.StringLike str, Selectable s str)
+select :: (TagSoup.StringLike str, Selectable str s)
        => s -> [TagSoup.Tag str] -> [[TagSoup.Tag str]]
 select s = selectNodes (toSelector s)
 
@@ -64,13 +67,16 @@ selectNode (SelectNode node attributes) tags = concatMap extractTagBlock nodes
 (@:) :: TagSoup.StringLike str
      => str -> [TagSoup.Attribute str] -> Selector str
 (@:) node attrs = [SelectNode node attrs]
+infixl 9 @:
 
 (@=) :: TagSoup.StringLike str => str -> str -> TagSoup.Attribute str
 (@=) node attrs = (node, attrs)
+infixl 6 @=
 
-(//) :: (TagSoup.StringLike str, Selectable a str, Selectable b str)
+(//) :: (TagSoup.StringLike str, Selectable str a, Selectable str b)
     => a -> b -> Selector str
 (//) a b = toSelector a ++ toSelector b
+infixl 5 //
 
 -- | Given a tag name and a list of attribute predicates return a function that
 -- returns true if a given tag matches the supplied name and predicates.
@@ -123,23 +129,56 @@ instance Monad (Scraper str) where
         where combined tags | (Just aVal) <- a tags = let (MkScraper b) = f aVal
                                                       in  b tags
                             | otherwise             = Nothing
+
 -- scrapeURL :: TagSoup.StringLike str => URL -> Scraper str a -> Maybe a
 -- scrapeURL = error "TODO"
 
--- recurse :: Selector -> Scraper a -> Scraper a
--- recurse = error "TODO"
---
--- recurses :: Selector -> Scraper a -> Scraper [a]
--- recurses = error "TODO"
---
--- innerText :: Selector -> Scraper T.Text
--- innerText = error "TODO"
---
--- innerTexts :: Selector -> Scraper [T.Text]
--- innerTexts = error "TODO"
---
--- attribute :: Selector -> Scraper T.Text
--- attribute = error "TODO"
---
--- attributes :: Selector -> Scraper [T.Text]
--- attributes = error "TODO"
+-- | TODO: Document this.
+chroot :: (TagSoup.StringLike str, Selectable str s)
+       => s -> Scraper str a -> Scraper str a
+chroot selector (MkScraper inner) = MkScraper
+                                  $ join . (inner <$>)
+                                  . listToMaybe . select selector
+
+-- | TODO: Document this.
+chroots :: (TagSoup.StringLike str, Selectable str s)
+        => s -> Scraper str a -> Scraper str [a]
+chroots selector (MkScraper inner) = MkScraper
+                                   $ return . catMaybes
+                                   . map inner . select selector
+
+-- | TODO: Document this.
+text :: (TagSoup.StringLike str, Selectable str s) => s -> Scraper str str
+text s = MkScraper $ withHead tagsToText . select s
+
+-- | TODO: Document this.
+texts :: (TagSoup.StringLike str, Selectable str s) => s -> Scraper str [str]
+texts s = MkScraper $ withAll tagsToText . select s
+
+-- | TODO: Document this.
+attr :: (Show str, TagSoup.StringLike str, Selectable str s)
+     => str -> s -> Scraper str str
+attr name s = MkScraper $ join . withHead (tagsToAttr name) . select s
+
+-- | TODO: Document this.
+attrs :: (Show str, TagSoup.StringLike str, Selectable str s)
+     => str -> s -> Scraper str [str]
+attrs name s = MkScraper $ fmap catMaybes . withAll (tagsToAttr name) . select s
+
+withHead :: (a -> b) -> [a] -> Maybe b
+withHead _ []    = Nothing
+withHead f (x:_) = Just $ f x
+
+withAll :: (a -> b) -> [a] -> Maybe [b]
+withAll _ [] = Nothing
+withAll f xs = Just $ map f xs
+
+tagsToText :: TagSoup.StringLike str => [TagSoup.Tag str] -> str
+tagsToText = TagSoup.innerText
+
+tagsToAttr :: (Show str, TagSoup.StringLike str)
+           => str -> [TagSoup.Tag str] -> Maybe str
+tagsToAttr attr tags = do
+    tag <- listToMaybe tags
+    guard $ TagSoup.isTagOpen tag
+    return $ TagSoup.fromAttrib attr tag
--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -1,15 +1,121 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Main (main) where
 
-import Test.HUnit
+import Control.Applicative
 import System.Exit
+import Test.HUnit
+import Text.HTML.Scalpel
 
-import qualified Text.HTML.ScalpelTest as ScalpelTest
+import qualified Text.HTML.TagSoup as TagSoup
 
 
 main = exit . failures =<< runTestTT (TestList [
-        ScalpelTest.tests
+        scrapeTests
+    ,   selectTests
     ])
 
 exit :: Int -> IO ()
 exit 0 = exitSuccess
 exit n = exitWith $ ExitFailure n
+
+selectTests = "selectTests" ~: TestList [
+        selectTest
+            ("a" @: [])
+            "<a>foo</a>"
+            ["<a>foo</a>"]
+
+    ,   selectTest
+            ("a" @: [])
+            "<a>foo</a><a>bar</a>"
+            ["<a>foo</a>", "<a>bar</a>"]
+
+    ,   selectTest
+            ("a" @: [])
+            "<b><a>foo</a></b>"
+            ["<a>foo</a>"]
+
+    ,   selectTest
+            ("a" @: [])
+            "<a><a>foo</a></a>"
+            ["<a><a>foo</a></a>", "<a>foo</a>"]
+
+    ,   selectTest
+            ("b" @: [])
+            "<a>foo</a>"
+            []
+
+    ,   selectTest
+            ("a" @: [])
+            "<a>foo"
+            []
+
+    ,   selectTest
+            ("a" @: ["key" @= "value"])
+            "<a>foo</a><a key=value>bar</a>"
+            ["<a key=value>bar</a>"]
+
+    ,   selectTest
+            ("a" // "b" @: [] // "c")
+            "<a><b><c>foo</c></b></a>"
+            ["<c>foo</c>"]
+
+    ,   selectTest
+            ("a" // "b")
+            "<c><a><b>foo</b></a></c><c><a><d><b>bar</b></d></a></c><b>baz</b>"
+            ["<b>foo</b>", "<b>bar</b>"]
+    ]
+
+selectTest :: (Show s, Selectable String s) => s -> String -> [String] -> Test
+selectTest selector tags expectedText = label ~: expected @=? actual
+    where
+        label  = "select (" ++ show selector ++ ") (" ++ show tags ++ ")"
+        expected = map TagSoup.parseTags expectedText
+        actual = select selector (TagSoup.parseTags tags)
+
+scrapeTests = "scrapeTests" ~: TestList [
+        scrapeTest
+            "<a>foo</a>"
+            (Just "foo")
+            (text "a")
+
+    ,   scrapeTest
+            "<a>foo</a><a>bar</a>"
+            (Just "foo")
+            (text "a")
+
+    ,   scrapeTest
+            "<a>foo</a><a>bar</a>"
+            (Just ["foo", "bar"])
+            (texts "a")
+
+    ,   scrapeTest
+            "<a>foo</a><a>bar</a>"
+            (Just [True, False])
+            (map (== "foo") <$> (texts $ "a"))
+
+    ,   scrapeTest
+            "<a key=foo />"
+            (Just "foo")
+            (attr "key" $ "a")
+
+    ,   scrapeTest
+            "<a key1=foo/><b key1=bar key2=foo /><a key1=bar key2=baz />"
+            (Just "baz")
+            (attr "key2" $ "a" @: ["key1" @= "bar"])
+
+    ,   scrapeTest
+            "<a><b>foo</b></a><b>bar</b>"
+            (Just ["foo"])
+            (chroot "a" $ texts "b")
+
+    ,   scrapeTest
+            "<a><b>foo</b></a><a><b>bar</b></a>"
+            (Just ["foo", "bar"])
+            (chroots "a" $ text "b")
+    ]
+
+scrapeTest :: (Eq a, Show a) => String -> Maybe a -> Scraper String a -> Test
+scrapeTest html expected scraper = label ~: expected @=? actual
+    where
+        label  = "scrape (" ++ show html ++ ")"
+        actual = scrape scraper (TagSoup.parseTags html)
