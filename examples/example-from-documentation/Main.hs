2a1205ffc3a6f1c9b6c778f8e07c9ad1d1cad6a5
Will Coster <willcoster@gmail.com>
2015-05-19 20:45:43 -0700

    (#6) Update and add examples
    The examples in the documentation to deriving the Show type class,
    without this GHCi will silently fail. Also add an examples directory
    with full compilable examples.

---
+++ examples/example-from-documentation/Main.hs
@@ -0,0 +1,51 @@
+import Text.HTML.Scalpel
+import Control.Applicative
+
+
+exampleHtml :: String
+exampleHtml = "<html>\
+\    <body>\
+\        <div class='comments'>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Sally</span>\
+\                <div class='comment text'>Woo hoo!</div>\
+\            </div>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Bill</span>\
+\                <img class='comment image' src='http://example.com/cat.gif' />\
+\            </div>\
+\            <div class='comment container'>\
+\                <span class='comment author'>Susan</span>\
+\                <div class='comment text'>WTF!?!</div>\
+\            </div>\
+\        </div>\
+\    </body>\
+\</html>"
+
+type Author = String
+
+data Comment
+    = TextComment Author String
+    | ImageComment Author URL
+    deriving (Show, Eq)
+
+main :: IO ()
+main = print $ scrapeStringLike exampleHtml comments
+    where
+    comments :: Scraper String [Comment]
+    comments = chroots ("div" @: [hasClass "container"]) comment
+
+    comment :: Scraper String Comment
+    comment = textComment <|> imageComment
+
+    textComment :: Scraper String Comment
+    textComment = do
+        author      <- text $ "span" @: [hasClass "author"]
+        commentText <- text $ "div"  @: [hasClass "text"]
+        return $ TextComment author commentText
+
+    imageComment :: Scraper String Comment
+    imageComment = do
+        author   <- text       $ "span" @: [hasClass "author"]
+        imageURL <- attr "src" $ "img"  @: [hasClass "image"]
+        return $ ImageComment author imageURL
