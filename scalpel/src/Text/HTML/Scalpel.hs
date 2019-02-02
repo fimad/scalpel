8cb18eb7636cce550cce62d63764720aad5c1dc0
ocramz <zocca.marco@gmail.com>
2019-02-02 17:00:19 +0100

    hyperlinks in haddock

--- scalpel/src/Text/HTML/Scalpel.hs
+++ scalpel/src/Text/HTML/Scalpel.hs
@@ -66,33 +66,35 @@
 -- The following snippet defines a function, @allComments@, that will download
 -- the web page, and extract all of the comments into a list:
 --
--- > type Author = String
--- >
--- > data Comment
--- >     = TextComment Author String
--- >     | ImageComment Author URL
--- >     deriving (Show, Eq)
--- >
--- > allComments :: IO (Maybe [Comment])
--- > allComments = scrapeURL "http://example.com/article.html" comments
--- >    where
--- >        comments :: Scraper String [Comment]
--- >        comments = chroots ("div" @: [hasClass "container"]) comment
--- >
--- >        comment :: Scraper String Comment
--- >        comment = textComment <|> imageComment
--- >
--- >        textComment :: Scraper String Comment
--- >        textComment = do
--- >            author      <- text $ "span" @: [hasClass "author"]
--- >            commentText <- text $ "div"  @: [hasClass "text"]
--- >            return $ TextComment author commentText
--- >
--- >        imageComment :: Scraper String Comment
--- >        imageComment = do
--- >            author   <- text       $ "span" @: [hasClass "author"]
--- >            imageURL <- attr "src" $ "img"  @: [hasClass "image"]
--- >            return $ ImageComment author imageURL
+-- @
+-- type Author = String
+--
+-- data Comment
+--     = TextComment Author String
+--     | ImageComment Author URL
+--     deriving (Show, Eq)
+--
+-- allComments :: IO (Maybe [Comment])
+-- allComments = 'scrapeURL' \"http:\/\/example.com/article.html\" comments
+--    where
+--        comments :: Scraper String [Comment]
+--        comments = 'chroots' ("div" '@:' ['hasClass' "container"]) comment
+--
+--        comment :: Scraper String Comment
+--        comment = textComment `<|>` imageComment
+--
+--        textComment :: Scraper String Comment
+--        textComment = do
+--            author      <- 'text' $ "span" @: [hasClass "author"]
+--            commentText <- text $ "div"  @: [hasClass "text"]
+--            return $ TextComment author commentText
+--
+--        imageComment :: Scraper String Comment
+--        imageComment = do
+--            author   <- text       $ "span" @: [hasClass "author"]
+--            imageURL <- 'attr' "src" $ "img"  @: [hasClass "image"]
+--            return $ ImageComment author imageURL
+-- @           
 --
 -- Complete examples can be found in the
 -- <https://github.com/fimad/scalpel/tree/master/examples examples> folder in
